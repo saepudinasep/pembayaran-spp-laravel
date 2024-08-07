54218c540be1592a22495033f60adfec91767921
saepudinasep <saepudinasep2001@gmail.com>
2024-08-07 08:44:34 +0700

    [ADD] reset password controller

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PasswordController extends Controller
 {
@@ -13,5 +15,25 @@ class PasswordController extends Controller
 
     public function process(Request $request)
     {
+        $request->validate([
+            'username' => 'required|string',
+            'passwordOld' => 'required|string',
+            'passwordNew' => 'required|string',
+        ]);
+
+        // dd($request);
+
+        $user = User::where('username', $request->username)->first();
+
+        if (!$user || !Hash::check($request->passwordOld, $user->password)) {
+            return back()->withErrors([
+                'username' => ['The provided credentials do not match our records.']
+            ]);
+        }
+
+        $user->password = Hash::make($request->passwordNew);
+        $user->save();
+
+        return redirect()->route('login')->with('status', 'Password successfully reset.');
     }
 }
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -18,7 +18,7 @@
                     <div class="form-group">
                         <input type="text" id="username" name="username"
                             class="form-control form-control-user @error('username') is-invalid @enderror"
-                            placeholder="Enter Email Address..." value="{{ old('username') }}" required autocomplete="off"
+                            placeholder="Enter Username..." value="{{ old('username') }}" required autocomplete="off"
                             autofocus>
 
                         @error('username')
@@ -32,8 +32,8 @@
                     <div class="form-group">
                         <input type="password" id="passwordOld" name="passwordOld"
                             class="form-control form-control-user @error('passwordOld') is-invalid @enderror"
-                            placeholder="Enter Email Address..." value="{{ old('passwordOld') }}" required
-                            autocomplete="off" autofocus>
+                            placeholder="Enter Old Password..." value="{{ old('passwordOld') }}" required autocomplete="off"
+                            autofocus>
 
                         @error('passwordOld')
                             <span class="invalid-feedback" role="alert">
@@ -46,8 +46,8 @@
                     <div class="form-group">
                         <input type="password" id="passwordNew" name="passwordNew"
                             class="form-control form-control-user @error('passwordNew') is-invalid @enderror"
-                            placeholder="Enter Email Address..." value="{{ old('passwordNew') }}" required
-                            autocomplete="off" autofocus>
+                            placeholder="Enter New Password..." value="{{ old('passwordNew') }}" required autocomplete="off"
+                            autofocus>
 
                         @error('passwordNew')
                             <span class="invalid-feedback" role="alert">
